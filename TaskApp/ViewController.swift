9ea6e57ea44bc68bee8aee7fc63df5b4e689e6b7
ryuuto.kimura <r_kimura@kyoei-e.com>
2024-10-15 16:33:35 +0900

    Initial Commit

---
+++ TaskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskApp
+//
+//  Created by mba2408.silver kyoei.engine on 2024/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
