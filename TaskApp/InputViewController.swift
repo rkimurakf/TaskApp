ee6ad3a2277fa2560399a93dd613dff81df6dd9f
ryuuto.kimura <r_kimura@kyoei-e.com>
2024-10-16 17:48:06 +0900

    pod installと.gitignoreの作業まで開始

---
+++ TaskApp/InputViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InputViewController.swift
+//  TaskApp
+//
+//  Created by mba2408.silver kyoei.engine on 2024/10/15.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var titleTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TaskApp/ViewController.swift
+++ TaskApp/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var viewTable: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        viewTable.fillerRowHeight = UITableView.automaticDimension
+        viewTable.delegate = self
+        viewTable.dataSource = self
     }
-
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection: section: Int) -> Int {
+        return 0
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexpath: IndexPath) -> UITableView {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        
+    }
 }
-
